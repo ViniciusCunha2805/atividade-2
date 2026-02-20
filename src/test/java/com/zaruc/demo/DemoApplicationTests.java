a04c583824ba431cc52ce536e9dc2041dac94550
vinicius.cunha <viniciussilvacunha2805@gmail.com>
2026-02-20 17:47:39 -0300

    feat(Tests): create automatics tests (started atividade-6 in atividade-2)

--- src/test/java/com/zaruc/demo/DemoApplicationTests.java
+++ src/test/java/com/zaruc/demo/DemoApplicationTests.java
@@ -1,13 +1,26 @@
 package com.zaruc.demo;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private UserRepository userRepository;
+
 	@Test
-	void contextLoads() {
+	@DisplayName("A ideia é salvar um usuário no banco de dados")
+	void deveSalvarUsuario() {
+		User usuario = new User();
+		usuario.setName("Estagiario Teste");
+		usuario.setLogin("teste_ti");
+
+		User usuarioSalvo = userRepository.save(usuario);
+
+		Assertions.assertNotNull(usuarioSalvo.getId());
+		Assertions.assertEquals("Estagiário Teste", usuarioSalvo.getName());
 	}
 
 }
